aaadcf4618b629d15dc963a27c10e4341c860657
Amol Garje <atulgarje222@gmail.com>
2022-07-05 19:51:13 +0530

    Done

---
+++ src/Components/Content.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+export class content extends Component {
+
+    render() {
+        return (
+            <>
+            <div className="card text-center ">
+                <div className="card" >
+                    <img class="Doni" src="images/Java.jpg" style={{width:'430px'}} ></img>
+                    <div className="card-body">
+                        <h5 className="card-title" >Amol Garje</h5>
+                        <p className="card-text">Co-Founder of company</p>
+                        <a href="/user" className="btn btn-primary">visit please</a>
+                    </div>
+                </div>
+                </div>
+
+            </>
+        )
+    }
+}
+
+export default content;
---
+++ src/Components/Footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export class Footer extends Component {
+
+    render() {
+        return (
+            <>
+                <div className="footer mt-auto border py-2 bg-dark " style={{ color: "white", backgroundColor: "black", textAlign: "center" }}>
+                <br />
+                <p> Amol_Garje &copy; All Rights Reserved.  </p>
+            </div>
+            </>
+        )
+    }
+}
+
+export default Footer;
